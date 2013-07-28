2b43287a46a91d8f19707264b7b9fee8107a1543
Jose Carmona <jcarmonaguerra@hotmail.com>
2013-07-28 18:52:27 -0500

    Empezando!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Autofrio::Application.config.secret_token = '3d5d33bc0689ef4a792cc6fef2121df8b9e20bbc1d7ada6d636f09b3406664c9fb9b26b0b788afec09366e244e9562a0578271314ad371c9459a21dcaf21d1a3'
