c075e59b3b4543f871f8acad9ffb11626f532077
Jose Carmona <jcarmonaguerra@hotmail.com>
2013-10-10 01:09:16 -0500

    Arreglando modelo de noticias

--- app/models/post.rb
+++ app/models/post.rb
@@ -21,7 +21,7 @@ class Post < ActiveRecord::Base
 	has_attached_file :image, :styles => { :large => "900x350>", :thumb => "360x140>" }, :default_url => "missing_:style.png"
 
 	validates :title, presence: true, uniqueness: true
-	validates :order, presence: true, uniqueness: true
+	validates :priority, presence: true, uniqueness: true
 	validates_attachment_size :image, :less_than => 5.megabytes
 	validates_attachment_content_type :image, :content_type => ['image/jpeg', 'image/png']
 
